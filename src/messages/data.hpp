006bed225ac1d8c7fd46aa59958b1b17af1737e4
Chih-Ta Lin <chitat@zillians.com>
2013-02-27 15:41:55 +0800

    Update src/messages/data.hpp
    add #include <boost/thread/lock_guard.hpp> for change to boost 1.53

--- src/messages/data.hpp
+++ src/messages/data.hpp
@@ -37,7 +37,9 @@
 #include <boost/function.hpp>
 #include <boost/intrusive_ptr.hpp>
 #include <boost/thread/mutex.hpp>
+#include <boost/thread/lock_guard.hpp>
 #include <boost/utility.hpp>
+
 
 #include <algorithm>
 #include <iostream>
